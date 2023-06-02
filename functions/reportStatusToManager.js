09707414cad36a5e65126f4d87c2602c9ec5e4bd
Maxim Stanovskiy <maxim.stanovskiy@gmail.com>
2023-06-02 17:36:30 +0300

    fix\reportStatusToManager: detail info

--- functions/reportStatusToManager.js
+++ functions/reportStatusToManager.js
@@ -1,7 +1,30 @@
 const axios = require("axios");
-const managerUrl = 'http://3.252.90.40:8000/fleet-manager'
+const managerUrl = "http://3.252.90.40:8000/fleet-manager";
+const simpleToken = "RNR-project-token";
 
 const reportStatusToManager = async () => {
-   await axios.get(managerUrl)
+  // -- form ec2 info --
+  const id = await (
+    await axios.get(`http://169.254.169.254/latest/meta-data/instance-id`)
+  ).data;
+
+  const region = await (
+    await axios.get(`http://169.254.169.254/latest/meta-data/placement/region`)
+  ).data;
+
+  const publicIp = await (
+    await axios.get(`http://169.254.169.254/latest/meta-data/public-ipv4`)
+  ).data;
+
+  await axios.post(
+    managerUrl,
+    { ip, region, publicIp },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + simpleToken,
+      },
+    }
+  );
 };
 module.exports = reportStatusToManager;
