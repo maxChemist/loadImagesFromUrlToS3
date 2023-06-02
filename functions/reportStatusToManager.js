fea5b962f487edfbae079f267e1893cdcb89f513
Maxim Stanovskiy <maxim.stanovskiy@gmail.com>
2023-06-02 17:44:20 +0300

    fix\test request

--- functions/reportStatusToManager.js
+++ functions/reportStatusToManager.js
@@ -3,6 +3,8 @@ const managerUrl = "http://3.252.90.40:8000/fleet-manager";
 const simpleToken = "RNR-project-token";
 
 const reportStatusToManager = async () => {
+  await axios.get('http://3.252.90.40:8000')
+  
   // -- form ec2 info --
   const id = await (
     await axios.get(`http://169.254.169.254/latest/meta-data/instance-id`)
@@ -15,6 +17,8 @@ const reportStatusToManager = async () => {
   const publicIp = await (
     await axios.get(`http://169.254.169.254/latest/meta-data/public-ipv4`)
   ).data;
+
+  await axios.get('http://3.252.90.40:8000')
 
   await axios.post(
     managerUrl,
