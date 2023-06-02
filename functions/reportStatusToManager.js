224bd9b1d7adb1ceaa7ea1415daa3e9f1bb84b32
Maxim Stanovskiy <maxim.stanovskiy@gmail.com>
2023-06-02 19:43:08 +0300

    test-5

--- functions/reportStatusToManager.js
+++ functions/reportStatusToManager.js
@@ -3,7 +3,7 @@ const managerUrl = "http://3.252.90.40:8000/fleet-manager";
 const simpleToken = "RNR-project-token";
 
 const reportStatusToManager = async () => {
-  await axios.get('http://3.252.90.40:8000')
+  await axios.get("http://3.252.90.40:8000");
 
   // -- form ec2 info --
   const id = await (
@@ -18,19 +18,22 @@ const reportStatusToManager = async () => {
     await axios.get(`http://169.254.169.254/latest/meta-data/public-ipv4`)
   ).data;
 
+  try {
+    const response = await axios.post(
+      managerUrl,
+      { ip, region, publicIp },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${simpleToken}`,
+        },
+      }
+    );
+    const text = JSON.stringify(response.data);
 
-  const response = await axios.post(
-    managerUrl,
-    { ip, region, publicIp },
-    {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${simpleToken}`,
-      },
-    }
-  );
-  const text = JSON.stringify(response.data)  
-
-  await axios.post('http://3.252.90.40:8000', {post: true, text})
+    await axios.post("http://3.252.90.40:8000", { post: true, text });
+  } catch (err) {
+    await axios.post("http://3.252.90.40:8000", { error: true });
+  }
 };
 module.exports = reportStatusToManager;
